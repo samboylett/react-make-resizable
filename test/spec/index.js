704a044ad8e7f9efa5d4e4d1dff82b32acea4d72
Sam Boylett <sam.boylett@piksel.com>
2018-07-01 11:10:27 +0100

    Revert "make sure it works when component updates UT"
    This reverts commit ade7874290b4a500cf6b97709337f8718119f586.

--- test/spec/index.js
+++ test/spec/index.js
@@ -120,18 +120,6 @@ describe('Resizer', () => {
 });
 
 describe('Resizable', () => {
-  const withWhenComponentUpdates = (tests) => {
-    tests();
-
-    describe('when component updates', () => {
-      beforeEach(() => {
-        component.update();
-      });
-
-      tests();
-    });
-  };
-
   describe('when passed more than 1 child', () => {
     it('throws an error', () => {
       expect(() => {
@@ -168,66 +156,62 @@ describe('Resizable', () => {
       ));
     });
 
-    withWhenComponentUpdates(() => {
-      it('renders the passed children', () => {
-        expect(component.find('div')).toHaveText('Foo bar');
-      });
+    it('renders the passed children', () => {
+      expect(component.find('div')).toHaveText('Foo bar');
+    });
 
-      it('sets the child position to relative', () => {
-        expect(component.find('div').instance().style.position).toEqual('relative');
-      });
+    it('sets the child position to relative', () => {
+      expect(component.find('div').instance().style.position).toEqual('relative');
     });
   });
 
   const mouseDownTests = (tests) => {
-    withWhenComponentUpdates(() => {
-      describe('when mouse down on resizer', () => {
+    describe('when mouse down on resizer', () => {
+      beforeEach(() => {
+        jest.spyOn(component.find('span').instance(), 'getBoundingClientRect')
+          .mockImplementation(() => ({
+            left: 10,
+            top: 10,
+            right: 50,
+            bottom: 50
+          }));
+
+        jest.spyOn(document.body, 'addEventListener');
+
+        const event = new MouseEvent('mousedown');
+
+        component.find(Resizer).find('div').instance().dispatchEvent(event);
+      });
+
+      ['mouseup', 'mousemove'].forEach((type) => {
+        it(`adds a ${type} event listener to the body`, () => {
+          expect(document.body.addEventListener)
+            .toHaveBeenCalledWith(type, expect.any(Function));
+        });
+      });
+
+      tests();
+
+      describe('when mouse up on body', () => {
         beforeEach(() => {
-          jest.spyOn(component.find('span').instance(), 'getBoundingClientRect')
-            .mockImplementation(() => ({
-              left: 10,
-              top: 10,
-              right: 50,
-              bottom: 50
-            }));
+          jest.spyOn(document.body, 'removeEventListener');
 
-          jest.spyOn(document.body, 'addEventListener');
+          const event = new MouseEvent('mouseup');
 
-          const event = new MouseEvent('mousedown');
-
-          component.find(Resizer).find('div').instance().dispatchEvent(event);
+          document.body.dispatchEvent(event);
         });
 
-        ['mouseup', 'mousemove'].forEach((type) => {
-          it(`adds a ${type} event listener to the body`, () => {
-            expect(document.body.addEventListener)
-              .toHaveBeenCalledWith(type, expect.any(Function));
-          });
+        it('sets the body cursor to auto', () => {
+          expect(document.body.style.cursor).toEqual('auto');
         });
 
-        tests();
+        ['mouseup', 'mousemove'].forEach((eventType) => {
+          it(`removes the ${eventType} event listener added the body`, () => {
+            const [, func] =
+              document.body.addEventListener.mock.calls.find(([type]) => type === eventType);
 
-        describe('when mouse up on body', () => {
-          beforeEach(() => {
-            jest.spyOn(document.body, 'removeEventListener');
-
-            const event = new MouseEvent('mouseup');
-
-            document.body.dispatchEvent(event);
-          });
-
-          it('sets the body cursor to auto', () => {
-            expect(document.body.style.cursor).toEqual('auto');
-          });
-
-          ['mouseup', 'mousemove'].forEach((eventType) => {
-            it(`removes the ${eventType} event listener added the body`, () => {
-              const [, func] =
-                document.body.addEventListener.mock.calls.find(([type]) => type === eventType);
-
-              expect(document.body.removeEventListener)
-                .toHaveBeenCalledWith(eventType, func);
-            });
+            expect(document.body.removeEventListener)
+              .toHaveBeenCalledWith(eventType, func);
           });
         });
       });
