d8a7fa6c361ef858958c8fc0d5e5568899243072
Sam Boylett <sam.boylett@piksel.com>
2018-07-01 15:51:42 +0100

    makeResizable implementation and storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -10,7 +10,9 @@ const specialAction = (name) =>
     return action(name)(...args);
   };
 
-import { Resizable, Resizer } from '../lib/index';
+import { Resizable, Resizer, makeResizable } from '../lib/index';
+
+const MadeResizable = makeResizable('div');
 
 storiesOf('Resizable', module)
   .addDecorator(story => (
@@ -63,4 +65,20 @@ storiesOf('Resizable', module)
         <Resizer position="right" thickness={5} style={{ background: 'blue' }} />
       </div>
     </Resizable>
+  ))
+  .add('using makeResizable', () => (
+    <MadeResizable
+      style={{
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '5px',
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}
+    >
+      Foo bar
+      <Resizer position="right" thickness={5} style={{ background: 'blue' }} />
+    </MadeResizable>
   ));
