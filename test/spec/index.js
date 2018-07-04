73b06203c21ca3a240208b6ccacd2ee13d08c05e
Sam Boylett <sam.boylett@piksel.com>
2018-07-04 15:38:49 +0100

    Revert "increase distance by thickness of resizer"
    This reverts commit 71875a9d642e1ce74e31987949222ae99b38e83c.

--- test/spec/index.js
+++ test/spec/index.js
@@ -221,6 +221,19 @@ describe('Resizable', () => {
     });
   };
 
+  describe('when passed more than 1 child', () => {
+    it('throws an error', () => {
+      expect(() => {
+        mount((
+          <Resizable>
+            <div>Foo</div>
+            <div>Bar</div>
+          </Resizable>
+        ));
+      }).toThrow();
+    });
+  });
+
   describe('when element position is set', () => {
     testsForComponent(
       () => ({}), (
